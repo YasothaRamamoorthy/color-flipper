d64046e6498644c12c8c2e9959fcf7368dd6925f
Yasotha Ramamoorthy <94780018+YasothaRamamoorthy@users.noreply.github.com>
2022-12-16 23:56:36 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn=document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click',function(){
+    //get random number between 0-3
+    const randomNumber=getRandomNumber();
+    console.loglog(randomNumber);
+    document.body.style.body.backgroundColor=colors[randomNumber];
+    color.textContent=colors[randomNumber]; 
+});
+
+function getRandomNumber()
+{
+    return Math.floor(Math.random()*colors.length);
+}

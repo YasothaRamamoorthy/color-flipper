1b4dd86a9ed20b5352bff8e1de2524020e700653
Yasotha Ramamoorthy <94780018+YasothaRamamoorthy@users.noreply.github.com>
2022-12-17 21:34:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const color = document.querySelector(".color");
 btn.addEventListener('click',function(){
     //get random number between 0-3
     const randomNumber=getRandomNumber();
-    console.loglog(randomNumber);
-    document.body.style.body.backgroundColor=colors[randomNumber];
+    console.log(randomNumber);
+    document.body.style.backgroundColor=colors[randomNumber];
     color.textContent=colors[randomNumber]; 
 });
 
 function getRandomNumber()
 {
-    return Math.floor(Math.random()*colors.length);
+    return Math.floor(Math.random() * colors.length);
 }
+
